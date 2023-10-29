9b6845f1f37b44cd2930b7af33133a3dbed9c315
yeison <yeisonvillegas07@gmail.com>
2023-10-29 15:19:18 -0500

    Create a web server

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,4,5]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return """
+        <h1>Hola soy una página</h1>
+        <p>Y soy un parrafo</p>
+    """
 
 def run():
     store.get_categories()
